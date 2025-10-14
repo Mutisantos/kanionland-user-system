12a1459ea61798275ee3341180f172c76cf6aba3
Esteban Hernández <mutisantos@hotmail.com>
2025-10-14 17:06:47 -0500

    Correct Database Test configuration + Github Action Workflow

--- src/test/java/com/kanionland/user/bff/TestDatabaseConfig.java
+++ src/test/java/com/kanionland/user/bff/TestDatabaseConfig.java
@@ -3,10 +3,13 @@ package com.kanionland.user.bff;
 
 import javax.sql.DataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
 
 @Configuration
 @Profile("test")
@@ -23,6 +26,20 @@ public class TestDatabaseConfig {
         .url("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
         .username(H2_DEFAULT_USERNAME)
         .password(H2_DEFAULT_PASSWORD)
+        .build();
+  }
+
+  @Bean
+  @Primary
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+      EntityManagerFactoryBuilder builder) {
+    return builder
+        .dataSource(dataSource())
+        .packages("com.kanionland.user.bff.domain.entities")
+        .persistenceUnit("test")
+        .properties(java.util.Collections.singletonMap(
+            "hibernate.hbm2ddl.auto", "create-drop"
+        ))
         .build();
   }
 }
